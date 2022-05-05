c1df3ae98a9c9302b1e3f749a0f22ea24a90dd56
williamskei97 <williamskei97@gmail.com>
2022-05-04 20:53:53 -0400

    Tested decision tree performance. 100% training accuracy and ~90% testing accuracy with max depth of 6.

--- RandomForest/Program.cs
+++ RandomForest/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RandomForest;
+
+Console.WriteLine("= = = Random Forest Algorithm = = =");
+
+LabeledData[] data = Util.ReadData("C:\\temp\\ionosphere.csv");
+data = Util.BalanceClasses(data);
+(var train, var test) = Util.Split(data);
+
+
+DecisionTree tree = new DecisionTree(2, 6);
+tree.Train(train);
+
+int numCorrect = 0;
+
+foreach(LabeledData d in train)
+{
+    int yPred = tree.Predict(d);
+    if(yPred == d.Label)
+    {
+        numCorrect++;
+    }
+}
+
+Console.WriteLine($"Decision tree train accuracy: {((double)numCorrect / train.Length):F3}, ({numCorrect}/{train.Length})");
+
+numCorrect = 0;
+
+foreach(LabeledData d in test)
+{
+    int yPred = tree.Predict(d);
+    if(yPred == d.Label)
+    {
+        numCorrect++;
+    }
+}
+
+Console.WriteLine($"Decision tree test accuracy: {((double)numCorrect / test.Length)}, ({numCorrect}/{test.Length})");
+
+Forest forest = new Forest();
+
---
+++ RandomForest/Util.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RandomForest
+{
+    internal class Util
+    {
+        public static LabeledData[] ReadData(string dir)
+        {
+            Dictionary<string, int> labelDict = new Dictionary<string, int>();
+            List<LabeledData> dataList = new List<LabeledData>();
+
+            using(FileStream stream = File.OpenRead(dir))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    int count = 0;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string? line = reader.ReadLine();
+                        if(count == 0 || line is null) 
+                        {
+                            count++;
+                            continue;
+                        }
+
+                        string[] splits = line.Split(',');
+                        int numCols = splits.Length;
+
+                        if(!labelDict.ContainsKey(splits[numCols - 1]))
+                        {
+                            labelDict.Add(splits[numCols - 1], labelDict.Count);
+                        }
+
+                        double[] features = new double[numCols - 1];
+                        for(int i = 0; i < numCols - 1; i++)
+                        {
+                            _ = double.TryParse(splits[i], out features[i]);
+                        }
+
+                        dataList.Add(new LabeledData(labelDict[splits[numCols - 1]], features));
+
+                        count++;
+                    }
+                }
+            }
+
+            return dataList.ToArray();
+        }
+
+        public static LabeledData[] BalanceClasses(LabeledData[] data)
+        {
+            List<int> classes = data.Select(x => x.Label).Distinct().ToList();
+            List<int> counts = new List<int>();
+
+            foreach(int c in classes)
+            {
+                counts.Add(data.Where(x => x.Label == c).Count());
+            }
+
+            int minCount = counts.Min();
+
+            Dictionary<int, int> classCounts = new Dictionary<int, int>();
+
+            foreach(int c in classes)
+            {
+                classCounts.Add(c, 0);
+            }
+
+            List<LabeledData> balancedData = new List<LabeledData>();
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                if(classCounts[data[i].Label] < minCount)
+                {
+                    balancedData.Add(data[i]);
+                    classCounts[data[i].Label]++;
+                }
+            }
+
+            return balancedData.ToArray();
+        }
+
+        public static (LabeledData[] train, LabeledData[] test) Split(LabeledData[] data, double trainAmount = 0.8)
+        {
+            int numTrain = (int)(data.Length * trainAmount);
+            Random random = new Random();
+
+            List<LabeledData> shuffled = data.OrderBy(x => random.Next()).ToList();
+            List<LabeledData> train = new List<LabeledData>();
+            List<LabeledData> test = new List<LabeledData>();
+
+            for(int i = 0; i < shuffled.Count; i++)
+            {
+                if(i < numTrain)
+                {
+                    train.Add(shuffled[i]);
+                }
+                else
+                {
+                    test.Add(shuffled[i]);
+                }
+            }
+
+            return (train.ToArray(), test.ToArray());
+        }
+    }
+}
